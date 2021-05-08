c984945662651c9ca1a4c453037a9425ba718e77
Deven3003 <83824216+Deven3003@users.noreply.github.com>
2021-05-08 23:57:41 +0530

    Add files via upload

---
+++ assignment 1/addition.java
@@ -0,0 +1,15 @@
+class addition{
+
+     public static void main(String []args){
+string s1=args[0];
+string s2=args[1];
+
+int i=Integer.parseInt(s1);
+int j=Integer.parseInt(s2);
+
+int k=i+j;
+
+System.out.println("addition="+k);
+}
+}
+
